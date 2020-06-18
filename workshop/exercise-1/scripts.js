6780818172755c5467e3591f944600c7d60d2280
Kanwar gill <gill.nihal@live.com>
2020-06-17 22:27:42 -0700

    Day-8_solutions

--- workshop/exercise-1/scripts.js
+++ workshop/exercise-1/scripts.js
@@ -1,1 +1,37 @@
-// Add your code here!
+const mainDiv = document.querySelector("main");
+
+const heading = document.createElement("h1");
+heading.innerText =
+  "The best How i met your Mother episode (according to fans)";
+mainDiv.appendChild(heading);
+
+const para1 = document.createElement("p");
+para1.innerText =
+  "As impossible as it seems to name just 1 of the 208 legendary episodes as the “best”, the fans have spoken and the highest rated episode is… wait forit…";
+mainDiv.appendChild(para1);
+
+const heading2 = document.createElement("h2");
+heading2.innerText = "The Slap Bet (Season 2, Episode 9)";
+mainDiv.appendChild(heading2);
+
+const para2 = document.createElement("p");
+para2.innerText = "IMDB Rating: 9.5";
+mainDiv.appendChild(para2);
+
+const image = document.createElement("img");
+image.src = "images/robin-sparkles.jpg";
+mainDiv.appendChild(image);
+
+const para3 = document.createElement("p");
+para3.innerText =
+  "In this episode, Ted is learning a few secrets about Robin, namely that she has an unexplained aversion to the mall. Robin refuses to tell Ted and the others why she won’t go to the mall, so the gang forms their own theories. Marshall believes it’s because she got married in a mall and is still married. Barney believes it has something to do with Robin having performed in a porn video. The two of them make a slap bet with each other: whoever is right gets to slap the other across the face as hard as he can. Lily is named the Slap Bet Commissioner, as long as she promises to be unbiased.";
+mainDiv.appendChild(para3);
+
+const para4 = document.createElement("p");
+para4.innerText =
+  "In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- available for all of eternity (horrible call Barney).";
+mainDiv.appendChild(para4);
+
+const image2 = document.createElement("a");
+image2.innerText = "Source";
+mainDiv.appendChild(image2);
--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,10 +1,47 @@
 // Preset values
 const FROGS = 3;
 
-// 1. Create for loop that makes use of FROGS to know how many lanes to create.
+const track = document.getElementById("track");
+//Create for loop that makes use of FROGS to know how many lanes to create.
+for (let laneNum = 1; laneNum <= FROGS; laneNum++) {
+  // create a list element li
+  let lane = document.createElement("li");
+  track.appendChild(lane);
+  // add span to track
+  let number = document.createElement("span");
+  number.innerText = laneNum;
+  track.appendChild(number);
+  //   assign id to each lane
+  lane.id = `frog-${laneNum}`;
+}
 
-// 2. Create li
+// empty array
+let racers = [];
+//frogs from frogstable pushed into empty array racers
+for (let i = 1; i <= FROGS; i++) {
+  const newFrog = frogStable[i];
+  racers.push(newFrog);
+}
+//console
+console.log(racers);
 
-// 3. Create span and add it to the li
+//
+racers.forEach(function (racer, id) {
+  //new span added
+  const newFrog = document.createElement("span");
+  //number of frog assigned
+  newFrog.innerText = `${racer.number}`;
+  //new class added to the frog
+  newFrog.classList.add("frog");
+  //background color added to the frog
+  newFrog.style.background = racer.color;
+  //newFrog appended to each lane
+  document.getElementById(`frog-${id + 1}`).appendChild(newFrog);
+  //progress
+  racers[id].progress = 0;
+  racers[id].lane = `frog-${id + 1}`;
 
-// 4. Assign an id to each lane
+  const frogProgress = document.createElement("span");
+  frogProgress.id = racers[id].name;
+  document.getElementById(`frog-${id + 1}`).appendChild(newFrog);
+});
